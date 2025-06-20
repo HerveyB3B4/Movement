7ce22b441a8990772a70d2e360e648406f65d69d
zerohzzzz <3190381602@qq.com>
2025-06-21 01:39:21 +0800

    feat: 添加目标跟踪逻辑，处理目标丢失情况

--- code/control/guide/guide.c
+++ code/control/guide/guide.c
@@ -1,8 +1,13 @@
 #include "guide.h"
 #include "image.h"
+#include "control.h"
 
 static int16 guide_target_vel = 0;
 static int16 guide_target_turn = 0;
+
+static int16 last_target_error = 0;
+static bool target_lost = false;
+static int16 search_turn_value = 1000; // 目标丢失时的转向强度
 
 void guide_set_target_vel(int16 target_vel)
 {
@@ -41,5 +46,33 @@ void guide_to_target(struct Control_Target *control_target)
     //     control_target->bottom_vel = -(float)guide_target_vel;
     // }
 
-    control_target->turn_err = get_img_target_error();
+    int16 current_error = get_img_target_error();
+
+    if (get_img_target_distance() > 0)
+    {
+        // 目标有效
+        target_lost = false;
+        last_target_error = current_error;
+        control_target->turn_err = current_error;
+    }
+    else
+    {
+        if (!target_lost)
+        {
+            target_lost = true;
+        }
+
+        if (last_target_error > 0)
+        {
+            control_target->turn_err = search_turn_value;
+        }
+        else if (last_target_error < 0)
+        {
+            control_target->turn_err = -search_turn_value;
+        }
+        else
+        {
+            control_target->turn_err = 0;
+        }
+    }
 }
--- code/control/guide/guide.h
+++ code/control/guide/guide.h
@@ -2,9 +2,11 @@
 #define _CONTROL_GUIDE_H
 
 #include "zf_common_headfile.h"
-#include "control.h"
 
 #define GUIDE_CONTANT_VEL -2000
+
+// 前向声明，避免循环包含
+struct Control_Target;
 
 int16 get_guide_target_vel(void);
 int16 get_guide_target_turn(void);
